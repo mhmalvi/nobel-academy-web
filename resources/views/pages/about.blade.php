45ba628ca4173b50a1f9b7aa27dac26262f9aabc
QuadQue <dev.quadque@gmail.com>
2021-05-12 11:22:00 +0600

    about page design modified

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -1,5 +1,17 @@
 @extends('layouts.app')
 
+@push('css')
+    <style>
+        .item h3 a{
+            color: #002B46 !important;
+        }
+
+        .item:hover h3 a{
+            color: #3FD4FB !important;
+        }
+    </style>
+@endpush
+
 @section('content')
     <!--Page Header-->
     <section class="page_header padding-top">
@@ -9,7 +21,7 @@
                     <h1>About NTA</h1>
                     <p>We offer certified courses in the beauty industry that you can rely on</p>
                     <div class="page_nav">
-                        <span>You are here:</span> <a href="index.html">Home</a> <span><i
+                        <span>You are here:</span> <a href="">Home</a> <span><i
                                 class="fa fa-angle-double-right"></i>About NTA</span>
                     </div>
                 </div>
@@ -43,7 +55,7 @@
                 </div>
                 <div class="col-md-5 wow fadeInRight" data-wow-delay="300ms">
                     <div class="image">
-                        <img src="{{ asset('assets/images/about-us.jpg') }}" alt="NTA">
+                        <img src="{{ asset('assets/images/about/about-01.svg') }}" alt="NTA">
                     </div>
                 </div>
             </div>
@@ -119,14 +131,69 @@
                 <h2 class="heading text-center">What We Do<span class="divider-center"></span></h2>
                 <div style="padding: 20px 0px;"></div>
                 <div class="row no-gutters">
+                    @php
+                    $DBT = 'This qualification reflects the role of individuals employed as beauty therapists to provide a broad range of beauty therapy treatments and services including facial treatments, body massage and treatments, hair removal, lash and brow treatments, nail and make-up services.';
+                    $CBT = 'This qualification reflects the role of individuals who work as beauty therapists to provide a range of beauty therapy treatments and services, including lash and brow treatments, nail services, make-up, massage and waxing.';
+                    $CHD = 'This qualification reflects the role of hairdressers who use a range of well-developed sales, consultation and technical skills and knowledge to provide a broad range of hairdressing services to clients.';
+                    $DRM = 'This qualification reflects the role of remedial massage therapists who work with clients presenting with soft tissue dysfunction, musculoskeletal imbalance or restrictions in range of motion (ROM).';
+                    @endphp
                     <div class="col-md-12">
-                        <div class="card-body">
-                            <div style="padding: 20px 0px;"></div>
-                            <p class="text-center" style="font-size: 1rem; line-height: 2;">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, 
-                                adipisci similique sequi culpa ad officia? Facilis ratione commodi natus sequi nam. 
-                                Totam sequi impedit aspernatur?
-                            </p>
+                        <div class="slider_wrapper">
+                            <div id="course_slider" class="owl-carousel">
+                                <div class="item">
+                                    <div class="image bottom20">
+                                        <img src="{{ asset('assets/images/courses/thumb01.jpg') }}" alt="Courses"
+                                            class="img-responsive border_radius">
+                                    </div>
+                                    <h3 class="bottom15"><a href="{{ route('diplomaInBeauty') }}" class="course_name">SHB50115
+                                            -
+                                            Diploma in Beauty Therapy</a></h3>
+                                    <p class="bottom15">
+                                        <?php echo mb_strimwidth($DBT, 0, 200, '...'); ?>
+                                    </p>
+                                    <a href="{{ route('diplomaInBeauty') }}" class="btn_common blue border_radius">Read
+                                        More</a>
+                                </div>
+                                <div class="item">
+                                    <div class="image bottom20">
+                                        <img src="{{ asset('assets/images/courses/thumb02.jpg') }}" alt="Courses"
+                                            class="img-responsive border_radius">
+                                    </div>
+                                    <h3 class="bottom15"><a href="{{ route('beautyTherapy') }}" class="course_name">SHB40115
+                                            -
+                                            Certificate IV in Beauty Therapy</a></h3>
+                                    <p class="bottom15">
+                                        <?php echo mb_strimwidth($CBT, 0, 200, '...'); ?>
+                                    </p>
+                                    <a href="{{ route('beautyTherapy') }}" class="btn_common blue border_radius">Read
+                                        More</a>
+                                </div>
+                                <div class="item">
+                                    <div class="image bottom20">
+                                        <img src="{{ asset('assets/images/courses/thumb03.jpg') }}" alt="Courses"
+                                            class="img-responsive border_radius">
+                                    </div>
+                                    <h3 class="bottom15"><a href="{{ route('hairdressing') }}" class="course_name">SHB30416 -
+                                            Certificate III in Hairdressing</a></h3>
+                                    <p class="bottom15">
+                                        <?php echo mb_strimwidth($CHD, 0, 200, '...'); ?>
+                                    </p>
+                                    <a href="{{ route('hairdressing') }}" class="btn_common blue border_radius">Read More</a>
+                                </div>
+                                <div class="item">
+                                    <div class="image bottom20">
+                                        <img src="{{ asset('assets/images/courses/thumb04.jpg') }}" alt="Courses"
+                                            class="img-responsive border_radius">
+                                    </div>
+                                    <h3 class="bottom15"><a href="{{ route('diplomaInRemedial') }}"
+                                            class="course_name">HLT52015 - Diploma in Remedial Massage</a></h3>
+                                    <p class="bottom15">
+                                        <?php echo mb_strimwidth($DRM, 0, 200, '...'); ?>
+                                    </p>
+                                    <a href="{{ route('diplomaInRemedial') }}" class="btn_common blue border_radius">Read
+                                        More</a>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
@@ -141,18 +208,45 @@
             <div class="card mb-3" style="background-color: transparent; border: 0px;">
                 <h2 class="heading text-center">How We Do<span class="divider-center"></span></h2>
                 <div style="padding: 20px 0px;"></div>
-                <div class="row no-gutters">
-                    <div class="col-md-12">
-                        <div class="card-body">
-                            <div style="padding: 30px 0px;"></div>
-                            <p class="text-center" style="font-size: 1; line-height: 2;">
-                                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, 
-                                adipisci similique sequi culpa ad officia? Facilis ratione commodi natus sequi nam. 
-                                Totam sequi impedit aspernatur?
-                            </p>
+                <p class="text-center">IT IS ALL COMPLETED IN FOLLOWING FIVE STEPS</p>
+                <div style="padding: 20px 0px;"></div>
+                <div class="row number-counters">
+                    <div class="col-md-3 col-sm-4">
+                        <div class="counters-item mb-5">
+                            <i class="icon-checkmark3"></i>
+                            <strong data-to="1">0</strong>
+                            <!-- Set Your Number here. i,e. data-to="56" -->
+                            <p>Unit Assesment Summery</p>
+                        </div>
+                        <div class="counters-item last mt-5">
+                            <i class="icon-checkmark3"></i>
+                            <strong data-to="2">0</strong>
+                            <p>Workbook</p>
                         </div>
                     </div>
-                </div>
+                    <div class="col-md-6 col-sm-4">
+                        <div class="fact-image">
+                            <img src="{{asset('assets/images/about/steps.png')}}" alt="some facts" class="img-responsive text-center">
+                        </div>
+                        <div class="counters-item">
+                            <i class="icon-checkmark3"></i>
+                            <strong data-to="3">0</strong>
+                            <p>Powerpoint Presentation</p>
+                        </div>
+                    </div>
+                    <div class="col-md-3 col-sm-4">
+                        <div class="counters-item">
+                            <i class="icon-checkmark3"></i>
+                            <strong data-to="5">0</strong>
+                            <p>Assesment</p>
+                        </div>
+                        <div class="counters-item last">
+                            <i class="icon-checkmark3"></i>
+                            <strong data-to="4">0</strong>
+                            <p>Resources</p>
+                        </div>
+                    </div>
+                  </div>
             </div>
         </div>
     </section>
