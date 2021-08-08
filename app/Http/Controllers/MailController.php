59466c6601cb5d9992ffb933ae2f01eba1bfb690
ANrajin <dev.quadque@gmail.com>
2021-08-08 11:54:16 +0600

    modified

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -1,117 +1,228 @@
 <?php
 
+
+
 namespace App\Http\Controllers;
 
+
+
 use Illuminate\Http\Request;
+
 use App\Http\Requests\ContactUsRequest;
+
 use App\Mail\ApplyNow;
+
 use App\Mail\ContactUs;
+
 use Illuminate\Support\Facades\Mail;
+
 use Illuminate\Support\Facades\Storage;
+
 use App\Mail\RplEligibilityMail;
+
 use App\Http\Requests\CheckEligibilityRequest;
 
+use App\Models\RplEligibilityRequest;
+
+
+
 class MailController extends Controller
+
 {
+
     /**
+
      * Contact Us mail
+
      */
+
     public function contactUs(ContactUsRequest $request)
+
     {
+
         $body = [
+
             'name' => $request->name,
+
             'email' =>  $request->email,
+
             'message' => $request->message,
+
         ];
+
+
 
         $subject = $request->subject;
 
+
+
         Mail::to('enquiries@nta.nsw.edu.au')->cc('info@quadque.tech')->send(new ContactUs($subject, $body));
 
+
+
         $notification = [
+
             'message'   =>  'Thanks for you query!',
+
             'alert-type'    =>  'info'
+
         ];
+
+
 
         return redirect()->back()->with($notification);
     }
+
+
+
 
 
     /**
+
      * Apply For course
+
      */
+
     public function courseApply(Request $request)
+
     {
+
         $body = [
+
             'name' => $request->name,
+
             'email' =>  $request->email,
+
             'contact' =>  $request->contact,
+
             'nationality' =>  $request->nationality,
+
             'course' =>  $request->course,
+
             'msg' => $request->msg
+
         ];
 
-        Mail::to('enquiries@nta.nsw.edu.au')->cc('info@quadque.tech')->send(new ApplyNow($body));
+
+
+        Mail::to('enquiries@nta.nsw.edu.au')->cc('tousif@quadque.tech')->send(new ApplyNow($body));
+
+
 
         $notification = [
+
             'message'   =>  'Thanks for your application',
+
             'alert-type'    =>  'info'
+
         ];
+
+
 
         return redirect()->back()->with($notification);
     }
 
 
-    public function checkEligibility(Request $request)
+
+
+
+    public function checkEligibility(CheckEligibilityRequest $request)
+
     {
+
         $directory = "temp/" . uniqid() . "/";
+
         $filePath = [];
 
+
+
         try {
+
             $data = [
+
                 'name' => $request->name,
+
                 'email' => $request->email,
+
                 'contact' => $request->contact,
+
                 'location' => $request->location,
+
                 'qualification' => $request->qualification,
+
                 'experience' => $request->experience,
+
                 'work_location' => $request->work_location,
+
                 'remark' => $request->remark
+
             ];
 
+
+
+            RplEligibilityRequest::create($data);
+
+
+
             if ($request->hasFile('files') && count($request->file('files')) > 0) {
+
                 $files = $request->file('files');
 
+
+
                 foreach ($files as $file) {
+
                     $fileName = $file->getClientOriginalName();
+
                     /**
+
                      * Check if derectory exist or not
+
                      * Create a new directory if not exist
+
                      */
 
+
+
                     if (!Storage::exists('public/' . $directory)) {
+
                         Storage::makeDirectory('public/' . $directory);
                     }
 
+
+
                     //store image into storage directory
+
                     Storage::putFileAs('public/' . $directory, $file, $fileName);
 
+
+
                     //store all the file name into the array
+
                     //so that we can pass it to mailable class
+
                     //mailable class will get the files and send it to mail
+
                     array_push($filePath, public_path('storage/' . $directory . $fileName));
                 }
             }
 
-            // Mail::to('enquiries@nta.nsw.edu.au')->cc('info@quadque.tech')->send(new RplEligibilityMail($data, $filePath));
-            Mail::to('dev.quadque@gmail.com')->cc('tousif@quadque.tech')->send(new RplEligibilityMail($data, $filePath));
+
+
+            Mail::to('enquiries@nta.nsw.edu.au')->cc('tousif@quadque.tech')->send(new RplEligibilityMail($data, $filePath));
+
+
 
             if ($request->hasFile('files') && count($request->file('files')) > 0) {
+
                 Storage::deleteDirectory('public/' . $directory);
             }
 
+
+
             return response()->json(['success' => 'success'], 200);
         } catch (\Throwable $th) {
+
             return response()->json(['error' => $th->getMessage()], 500);
         }
     }
---
+++ database/migrations/2021_08_02_044527_create_rpl_eligibility_requests_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRplEligibilityRequestsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rpl_eligibility_requests', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email');
+            $table->string('contact');
+            $table->string('location');
+            $table->string('qualification');
+            $table->string('relevant');
+            $table->string('work_location');
+            $table->string('designation');
+            $table->string('industry');
+            $table->string('course');
+            $table->longText('remark')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rpl_eligibility_requests');
+    }
+}
