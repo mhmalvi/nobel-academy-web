51b28f08d961bdc8201afe6a5abd8f6046da9ecb
Zakaria <jakariablaine120@gmail.com>
2021-12-22 18:33:29 +0600

    Add more content in faq page

--- resources/views/pages/faq.blade.php
+++ resources/views/pages/faq.blade.php
@@ -28,48 +28,73 @@
                     <h2 class="heading heading_space">FAQ <span class="divider-left"></span></h2>
                     <section class="accordion-section clearfix mt-3" aria-label="Question Accordions">
                         <div class="container">
-                            <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
-                                <div class="panel panel-default">
-                                    <div class="panel-heading p-3 mb-3" role="tab" id="faq1">
-                                        <h3 class="panel-title">
-                                        <a class="collapsed" role="button" title="" data-toggle="collapse" href="#collapse1" aria-expanded="true" aria-controls="collapse1">
-                                            Question #1 <i class="pull-right fa fa-plus"></i>
-                                        </a>
-                                        </h3>
-                                    </div>
-                                    <div id="collapse1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="faq1">
-                                        <div class="panel-body px-3 mb-4">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Error facilis iure obcaecati perferendis labore commodi? Eos magnam similique eum asperiores pariatur impedit, minus quas possimus, sunt voluptas exercitationem quod? Porro?
+                            <div class="row">
+                                <div class="col-md-8">
+                                    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="true">
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading p-3 mb-3" role="tab" id="faq1">
+                                                <h3 class="panel-title">
+                                                <a class="collapsed" role="button" title="" data-toggle="collapse" href="#collapse1" aria-expanded="true" aria-controls="collapse1">
+                                                    Question #1 <i class="pull-right fa fa-plus"></i>
+                                                </a>
+                                                </h3>
+                                            </div>
+                                            <div id="collapse1" class="panel-collapse collapse" role="tabpanel" aria-labelledby="faq1">
+                                                <div class="panel-body px-3 mb-4">
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Error facilis iure obcaecati perferendis labore commodi? Eos magnam similique eum asperiores pariatur impedit, minus quas possimus, sunt voluptas exercitationem quod? Porro?
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading p-3 mb-3" role="tab" id="faq2">
+                                                <h3 class="panel-title">
+                                                <a class="collapsed" role="button" title="" data-toggle="collapse" href="#collapse2" aria-expanded="true" aria-controls="collapse2">
+                                                    Question #2 <i class="pull-right fa fa-plus"></i>
+                                                </a>
+                                                </h3>
+                                            </div>
+                                            <div id="collapse2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="faq2">
+                                                <div class="panel-body px-3 mb-4">
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere provident perferendis harum ex dolore similique, ratione tenetur odit veniam earum ipsam quia? Provident repellendus cupiditate accusantium sunt a pariatur dolorum.
+                                                </div>
+                                            </div>
+                                        </div>
+
+                                        <div class="panel panel-default">
+                                            <div class="panel-heading p-3 mb-3" role="tab" id="faq3">
+                                                <h3 class="panel-title">
+                                                <a class="collapsed" role="button" title="" data-toggle="collapse" href="#collapse3" aria-expanded="true" aria-controls="collapse3">
+                                                    Question #3 <i class="pull-right fa fa-plus"></i>
+                                                </a>
+                                                </h3>
+                                            </div>
+                                            <div id="collapse3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="faq3">
+                                                <div class="panel-body px-3 mb-4">
+                                                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere provident perferendis harum ex dolore similique, ratione tenetur odit veniam earum ipsam quia? Provident repellendus cupiditate accusantium sunt a pariatur dolorum.
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
 
-                                <div class="panel panel-default">
-                                    <div class="panel-heading p-3 mb-3" role="tab" id="faq2">
-                                        <h3 class="panel-title">
-                                        <a class="collapsed" role="button" title="" data-toggle="collapse" href="#collapse2" aria-expanded="true" aria-controls="collapse2">
-                                            Question #2 <i class="pull-right fa fa-plus"></i>
-                                        </a>
+                                <div class="col-md-4">
+                                    <div class="card learn-more-faq">
+                                        <h3 class="card-header">
+                                            Looking for answer you can't find?
                                         </h3>
-                                    </div>
-                                    <div id="collapse2" class="panel-collapse collapse" role="tabpanel" aria-labelledby="faq2">
-                                        <div class="panel-body px-3 mb-4">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere provident perferendis harum ex dolore similique, ratione tenetur odit veniam earum ipsam quia? Provident repellendus cupiditate accusantium sunt a pariatur dolorum.
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="panel panel-default">
-                                    <div class="panel-heading p-3 mb-3" role="tab" id="faq3">
-                                        <h3 class="panel-title">
-                                        <a class="collapsed" role="button" title="" data-toggle="collapse" href="#collapse3" aria-expanded="true" aria-controls="collapse3">
-                                            Question #3 <i class="pull-right fa fa-plus"></i>
-                                        </a>
-                                        </h3>
-                                    </div>
-                                    <div id="collapse3" class="panel-collapse collapse" role="tabpanel" aria-labelledby="faq3">
-                                        <div class="panel-body px-3 mb-4">
-                                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere provident perferendis harum ex dolore similique, ratione tenetur odit veniam earum ipsam quia? Provident repellendus cupiditate accusantium sunt a pariatur dolorum.
+                                        <div class="card-body">
+                                            <div class="row">
+                                                <div class="col-xs-8">
+                                                    Contact our hotline -
+                                                    <a href="tel:1300 616 123">
+                                                        1300 616 123
+                                                    </a>
+                                                </div>
+                                                <div class="col-xs-4">
+                                                    <img src="/assets/images/headphone.png" alt="call us">
+                                                </div>
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
@@ -106,11 +131,13 @@
 
         .panel-heading h3 a:not(.collapsed)
         {
-            color: #002b46 !important;
+            color: #fff !important;
+            background: #3fd4fb !important;
         }
 
         .panel-heading h3 a:not(.collapsed) i:before {
             content: "\f068";
+            color: #fff;
         }
         .panel-heading h3 a i {
             color: #002b46;
@@ -118,6 +145,24 @@
 
         .panel-body {
             padding: 20px 30px;
+        }
+
+        .learn-more-faq {
+            border: 1px solid #d8d8d8 !important;
+        }
+
+        .learn-more-faq .card-header {
+            background: #3fd4fb !important;
+            color: #fff;
+            border-width: 0;
+        }
+
+        .learn-more-faq .card-body .row {
+            margin: 0 !important;
+        }
+
+        .learn-more-faq .card-body img {
+            width: 100%;
         }
     </style>
 @endpush
