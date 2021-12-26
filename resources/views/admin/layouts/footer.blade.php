e3fb2fda312a928ab4f87695f8376f02ed76a6f1
ANrajin <dev.quadque@gmail.com>
2021-12-26 17:17:29 +0600

    dashboard layout updated

---
+++ resources/views/admin/layouts/footer.blade.php
@@ -0,0 +1,3 @@
+<div class="footer-content">
+    <p class="m-b-0">Copyright © 2021 Quadque Technologies ltd. All rights reserved.</p>
+</div>
---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,39 @@
+<div class="side-nav">
+    <div class="side-nav-inner">
+        <ul class="side-nav-menu scrollable">
+            <li class="nav-item">
+                <a href="javascript:void(0);">
+                    <span class="icon-holder">
+                        <i class="anticon anticon-dashboard"></i>
+                    </span>
+                    <span class="title">Dashboard</span>
+                </a>
+            </li>
+            <li class="nav-item dropdown open">
+                <a class="dropdown-toggle" href="javascript:void(0);">
+                    <span class="icon-holder">
+                        <i class="anticon anticon-dashboard"></i>
+                    </span>
+                    <span class="title">Dashboard</span>
+                    <span class="arrow">
+                        <i class="arrow-icon"></i>
+                    </span>
+                </a>
+                <ul class="dropdown-menu">
+                    <li class="active">
+                        <a href="index.html">Default</a>
+                    </li>
+                    <li>
+                        <a href="index-crm.html">CRM</a>
+                    </li>
+                    <li>
+                        <a href="index-e-commerce.html">E-commerce</a>
+                    </li>
+                    <li>
+                        <a href="index-projects.html">Projects</a>
+                    </li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</div>
