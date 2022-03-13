1bb6d07c1f5b36598b1af0781976de9cf6790639
Zakaria <jakariablaine120@gmail.com>
2022-03-13 16:10:17 +0600

    Update pixel code

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,5 +1,6 @@
 <!doctype html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
 <head>
     <meta name="google-site-verification" content="" />
     <meta charset="utf-8">
@@ -10,10 +11,14 @@
     <title>@yield('title') - NTA</title>
     <link rel="icon" href="{{ asset('assets/images/logo/favicon.png') }}">
 
-    <link rel="preload" href="{{asset('assets/fonts/museo_slab_0-webfont.woff2')}}" as="font" type="font/woff2" crossorigin>
-    <link rel="preload" href="{{asset('assets/fonts/museo_slab_0-webfont.woff')}}" as="font" type="font/woff" crossorigin>
-    <link rel="preload" href="{{asset('assets/fonts/museo_slab_1-webfont.woff2')}}" as="font" type="font/woff2" crossorigin>
-    <link rel="preload" href="{{asset('assets/fonts/museo_slab_1-webfont.woff')}}" as="font" type="font/woff" crossorigin>
+    <link rel="preload" href="{{ asset('assets/fonts/museo_slab_0-webfont.woff2') }}" as="font" type="font/woff2"
+        crossorigin>
+    <link rel="preload" href="{{ asset('assets/fonts/museo_slab_0-webfont.woff') }}" as="font" type="font/woff"
+        crossorigin>
+    <link rel="preload" href="{{ asset('assets/fonts/museo_slab_1-webfont.woff2') }}" as="font" type="font/woff2"
+        crossorigin>
+    <link rel="preload" href="{{ asset('assets/fonts/museo_slab_1-webfont.woff') }}" as="font" type="font/woff"
+        crossorigin>
 
     <link rel="canonical" href="{{ URL::current() }}" />
     <meta name="robots" content="follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large" />
@@ -42,29 +47,41 @@
     <script async src="https://www.googletagmanager.com/gtag/js?id=G-ZFG86J3Y2L"></script>
 
     <script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
+        window.dataLayer = window.dataLayer || [];
 
-    gtag('config', 'G-ZFG86J3Y2L');
+        function gtag() {
+            dataLayer.push(arguments);
+        }
+        gtag('js', new Date());
+
+        gtag('config', 'G-ZFG86J3Y2L');
     </script>
 
     <!-- Meta Pixel Code -->
     <script>
-    !function(f,b,e,v,n,t,s)
-    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-    n.queue=[];t=b.createElement(e);t.async=!0;
-    t.src=v;s=b.getElementsByTagName(e)[0];
-    s.parentNode.insertBefore(t,s)}(window, document,'script',
-    'https://connect.facebook.net/en_US/fbevents.js');
-    fbq('init', '938959763406519');
-    fbq('track', 'PageView');
+        ! function(f, b, e, v, n, t, s) {
+            if (f.fbq) return;
+            n = f.fbq = function() {
+                n.callMethod ?
+                    n.callMethod.apply(n, arguments) : n.queue.push(arguments)
+            };
+            if (!f._fbq) f._fbq = n;
+            n.push = n;
+            n.loaded = !0;
+            n.version = '2.0';
+            n.queue = [];
+            t = b.createElement(e);
+            t.async = !0;
+            t.src = v;
+            s = b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t, s)
+        }(window, document, 'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+        fbq('init', '3896181297274286');
+        fbq('track', 'PageView');
     </script>
     <noscript><img height="1" width="1" style="display:none"
-    src="https://www.facebook.com/tr?id=938959763406519&ev=PageView&noscript=1"
-    /></noscript>
+            src="https://www.facebook.com/tr?id=3896181297274286&ev=PageView&noscript=1" /></noscript>
     <!-- End Meta Pixel Code -->
 
     @include('partials.styles')
@@ -92,7 +109,7 @@
     @endif
 
     {{-- <div id="app"> --}}
-        @yield('content')
+    @yield('content')
     {{-- </div> --}}
 
     @include('components.rpl-modal')
@@ -101,7 +118,7 @@
     {{-- <script src="{{ asset('js/app.js') }}"></script> --}}
     @include('partials.scripts')
     @include('components.applyNow')
-    
+
 </body>
 
 </html>
